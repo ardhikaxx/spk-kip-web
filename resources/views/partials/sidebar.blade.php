4d9796ee9fecc059ce5fe6ef5da34685e44ea120
ardhikaxx <ardhikayanuar58@gmail.com>
2026-05-27 21:59:54 +0700

    fix: perbaruan menu sidebar untuk Kategorisasi Kriteria

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
             <a class="sidebar-menu-item {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}" href="{{ route('admin.dashboard') }}"><i class="bi bi-graph-up"></i> Dashboard</a>
             <a class="sidebar-menu-item {{ request()->routeIs('mahasiswa.*') ? 'active' : '' }}" href="{{ route('mahasiswa.index') }}"><i class="bi bi-person-vcard"></i> Data Mahasiswa</a>
             <a class="sidebar-menu-item {{ request()->routeIs('kriteria.*') ? 'active' : '' }}" href="{{ route('kriteria.index') }}"><i class="bi bi-file-earmark-text"></i> Data Kriteria</a>
-            <a class="sidebar-menu-item {{ request()->routeIs('sub-kriteria.*') ? 'active' : '' }}" href="{{ route('sub-kriteria.index') }}"><i class="bi bi-folder2-open"></i> Data Subkriteria</a>
+            <a class="sidebar-menu-item {{ request()->routeIs('kategorisasi-kriteria.*') ? 'active' : '' }}" href="{{ route('kategorisasi-kriteria.index') }}"><i class="bi bi-folder2-open"></i> Kategorisasi Kriteria</a>
             <a class="sidebar-menu-item {{ request()->routeIs('bobot.*') ? 'active' : '' }}" href="{{ route('bobot.index') }}"><i class="bi bi-gear"></i> Pengaturan Bobot</a>
             <a class="sidebar-menu-item {{ request()->routeIs('alternatif.*') ? 'active' : '' }}" href="{{ route('alternatif.index') }}"><i class="bi bi-file-earmark-spreadsheet"></i> Kelola Alternatif</a>
             <a class="sidebar-menu-item {{ request()->routeIs('promethee.*') ? 'active' : '' }}" href="{{ route('promethee.index') }}"><i class="bi bi-calculator"></i> Hitung Promethee</a>
